feb99b9af1e38948e6ca3a926cdcfc56a3fae5fe
Matheus Henrique Santos Da Silva <=>
2021-01-27 16:57:16 -0300

    REACT LIST

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function ToDoList() {
   const [taskList, setTaskList] = useState([
@@ -12,6 +12,17 @@ function ToDoList() {
       description: "Estudar React",
     },
   ]);
+
+  const [inputTask, setInputTask] = useState({ id: "", description: "" });
+
+  window.addEventListener("beforeunload", () => {
+    localStorage.setItem("lista", JSON.stringify(taskList));
+  });
+
+  useEffect(() => {
+    setTaskList(JSON.parse(localStorage.getItem("lista")) || []);
+    
+  }, []);
 
   const handleInsert = (description) => {
     const newId =
@@ -28,54 +39,93 @@ function ToDoList() {
     setTaskList(taskList.filter((task) => task.id !== id));
   };
 
+  const handleEdit = (task) => {
+    setInputTask(task);
+  };
+
+  const handleSaveEdit = () => {
+    setTaskList(
+      taskList.map((task) => (task.id === inputTask.id ? inputTask : task))
+    );
+  };
+
   return (
     <div className="container">
-      <Form handleInsert={handleInsert} />
+      <Form
+        handleInsert={handleInsert}
+        newTask={inputTask}
+        setNewTask={setInputTask}
+        handleSaveEdit={handleSaveEdit}
+      />
       <hr />
-      <List list={taskList} handleRemove={handleRemove} />
+      <List
+        list={taskList}
+        handleRemove={handleRemove}
+        handleEdit={handleEdit}
+      />
     </div>
   );
 }
 
-function Form({ handleInsert }) {
-  const [newTask, setNewTask] = useState("");
-
+function Form({ handleInsert, newTask, setNewTask, handleSaveEdit }) {
   const handleNewTask = (e) => {
-    setNewTask(e.target.value);
+    setNewTask({ ...newTask, description: e.target.value });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    handleInsert(newTask);
+    if (newTask.id === "") {
+      handleInsert(newTask.description);
+    } else {
+      handleSaveEdit();
+    }
+
+    setNewTask({ id: "", description: "" });
   };
 
   return (
     <form className="form" onSubmit={handleSubmit}>
-      <input type="text" value={newTask} onChange={handleNewTask} required />
+      <input
+        type="text"
+        value={newTask.description}
+        onChange={handleNewTask}
+        required
+      />
       <button>Ok!</button>
     </form>
   );
 }
 
-function List({ list, handleRemove }) {
+function List({ list, handleRemove, handleEdit }) {
   return (
     <section>
-        {list.length === 0 && "Você não tem tarefas"}
-      {list.map((item) => (
-        <Item task={item} handleRemove={handleRemove} />
+      {list.length === 0 && <p className="withoutTask">Você não tem tarefas</p>}
+      {list.map((item, index) => (
+        <Item
+          key={item.id}
+          task={item}
+          index={index}
+          handleRemove={handleRemove}
+          handleEdit={handleEdit}
+        />
       ))}
     </section>
   );
 }
 
-function Item({ task, handleRemove }) {
+function Item({ task, handleRemove, handleEdit, index }) {
   return (
     <article className="item">
       <p>
-        {task.id} - {task.description}
+        {index + 1} - {task.description}
       </p>
-      <span onClick={() => handleRemove(task.id)}>&times;</span>
+      <div>
+        <span style={{ fontSize: 25 }} onClick={() => handleEdit(task)}>
+          &#9998;
+        </span>
+        <span onClick={() => handleRemove(task.id)}>&times;</span>
+      </div>
     </article>
   );
 }
